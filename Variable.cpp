32992ac95ac1ce229a4bca5e52d3ee918da810d0
Jerry <111554668+NotTheRussian@users.noreply.github.com>
2022-11-13 22:24:35 -0600

    Add files via upload

---
+++ Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string PlayerName = "Player" ;
+    cout << "The " << PlayerName << " played for years," << endl;
+    cout << "and with the practice, he won the Tournament." << endl;
+    return 0;
+}
